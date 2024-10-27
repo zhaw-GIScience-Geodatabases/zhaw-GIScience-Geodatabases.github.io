5ef83aa88558f578801b4cae290bf2de792721dc
ratnanil <rata@zhaw.ch>
2024-10-27 23:04:28 +0100

    udpate

---
+++ crop_images.sh
@@ -0,0 +1,7 @@
+convert images/simple_feature_types.svg -crop 150x180+0+0 images/point.png
+convert images/simple_feature_types.svg -crop 150x180+150+0 images/linestring.png
+convert images/simple_feature_types.svg -crop 150x180+300+0 images/polygon.png
+convert images/simple_feature_types.svg -crop 150x180+0+180 images/multipoint.png
+convert images/simple_feature_types.svg -crop 150x180+150+180 images/multilinesting.png
+convert images/simple_feature_types.svg -crop 150x180+300+180 images/multipolygon.png
+convert images/simple_feature_types.svg -crop 150x180+450+180 images/geometrycollection.png
